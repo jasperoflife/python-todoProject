900ca4faf6f529fc5b206b02691dd7a5d3d1aed1
Iyeyemi Akinrinlola <iyeyemigodswill@gmail.com>
2022-12-15 06:32:35 +0100

    my fifth commit

--- members/views.py
+++ members/views.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm #for sign up form
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.contrib import messages #for success or error msg
 # Create your views here.
 
 def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
+        password = request.POST['password'] #for regular user
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -15,7 +16,30 @@ def login_user(request):
         
         else:
             # Return an 'invalid login' error message.
-            messages.success(request, ('There was an error logging in, try again!'))
+            messages.success(request, ('There was an error, try again!'))
             return redirect("login")
     else:
         return render(request, 'authenticate/login.html', {})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You were logged out!'))
+    return redirect('/')
+    
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            username = request.POST['username']
+            password = request.POST['password1'] #for regular user
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Successfully Signed Up!'))
+            return redirect('/')
+        
+    else:
+        form = UserCreationForm
+        
+    return render(request, 'authenticate/signup.html', {'form':form,})
